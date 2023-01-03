9afc69c87eefa5c0a76b89cfa45ae8bd6399ac08
Hewlett Packard <121778515+Hewlett-Packard-Helper@users.noreply.github.com>
2023-01-02 19:38:51 -0700

    Update pull.js

--- commands/pull.js
+++ commands/pull.js
@@ -1,6 +1,10 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const { ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { channel } = require('diagnostics_channel');
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
 async function getDataFromPage(url) {
   const response = await fetch(url);
@@ -14,20 +18,42 @@ async function getDataFromPage(url) {
 module.exports = {
   data: new SlashCommandBuilder()
       .setName('pull')
-      .setDescription('Pulls data from a website using cheerio'),
-  async execute(interaction) {
-    const url = 'https://example.com';
-    const data = await getDataFromPage(url);
-    const exampleEmbed = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setTitle(`${interaction.member.user.username}`)
-      .setAuthor({ name: 'HewlettHelper', iconURL: 'https://i.imgur.com/BndcSnQ.png'})
-      .setDescription(`The server is ${interaction.guild.name}`)
-      .addFields(
-        { name: `${interaction.member.user.username}`, value:  `${interaction.member.user.username} Joined on ${new Date(interaction.member.joinedAt).toUTCString()}` },
-        { name: 'Header', value: data.header },
-        { name: 'Paragraph', value: data.paragraph },
-      )
-      .setImage(`${interaction.member.user.displayAvatarURL()}`)
-	  }
-	}
+      .setDescription('Pulls data from a website'),
+  async execute(interaction, client) {
+    // Create the modal
+    const modal = new ModalBuilder()
+  		.setTitle('My Modal')
+  		.setCustomId('myModal')
+
+    // Send the modal and wait for a response
+    const favoriteColorInput = new TextInputBuilder()
+			.setCustomId('favoriteColorInput')
+		    // The label is the prompt the user sees for this input
+			.setLabel("What's your favorite color?")
+		    // Short means only a single line of text
+			.setStyle(TextInputStyle.Short);
+
+		const hobbiesInput = new TextInputBuilder()
+			.setCustomId('hobbiesInput')
+			.setLabel("What's some of your favorite hobbies?")
+		    // Paragraph means multiple lines of text.
+			.setStyle(TextInputStyle.Paragraph);
+
+		// An action row only holds one text input,
+		// so you need one action row per text input.
+		const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+		const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+
+		// Add inputs to the modal
+		modal.addComponents(firstActionRow, secondActionRow);
+    // Get the URL from the modal response
+    await interaction.showModal(modal)
+
+    // Get the data from the page
+	client.on(Events.InteractionCreate, interaction => {
+		if (!interaction.isModalSubmit()) return;
+		console.log(interaction);
+	});
+    // Send the data in an embed
+  }
+}
